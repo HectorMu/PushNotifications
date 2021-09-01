7dcb6718ac530af33dee00f6f29d7e06d75767c9
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-09-01 10:38:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,15 +11,6 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use (function (req, res, next) {
-    if (req.secure) {
-            // request was via https, so do no special handling
-            next();
-    } else {
-            // request was via http, so redirect to https
-            res.redirect('https://' + req.headers.host + req.url);
-    }
-});
 
 
 //routes
