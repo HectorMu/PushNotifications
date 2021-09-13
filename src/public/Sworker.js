2804fc709b466d2a99c7f39072a95bcc6db573f9
HectorMu <42363909+HectorMu@users.noreply.github.com>
2021-09-12 20:21:18 -0500

    Update Sworker.js

--- src/public/Sworker.js
+++ src/public/Sworker.js
@@ -1,9 +1,8 @@
-
 self.addEventListener('push',e =>{
     const data = e.data.json()
     console.log(data);
     self.registration.showNotification(data.title,{
-        body: data.message,
+        body: data.message, 
         icon:'https://pbs.twimg.com/profile_images/1280149050965331968/MmdsLqBZ_400x400.jpg'
     })
 })
